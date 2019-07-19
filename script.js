837cc8654080d2e104dc77faee0bd5ed40494d15
renan campos <camposrenanrc@gmail.com>
2019-07-19 17:42:24 -0300

    adicionar atividade

---
+++ script.js
@@ -0,0 +1,93 @@
+// let idade = Number(prompt("Digite sua idade"));
+
+// let aposentado = prompt("Você é aposentado?");
+
+// let estudante = prompt("Você é estudante?");
+
+// if(idade >= 60 || aposentado == "sim"){
+//     alert("Entrada Grátis");
+// }
+
+// else if(Aposentado = sim){
+//     alert("Entrada Grátis");
+// }
+// else if(idade >= 0 && idade <= 12){
+//     alert("Sua Entrada é R$10,00");
+// }
+// else if(idade >= 13 && idade <= 17){
+//     alert("Sua Entrada é R$15,00");
+// }
+// else if(idade >= 18 && idade <= 59){
+//     if(estudante === "sim"){
+//    alert("Entrada R$17,50 meia entrada");
+//     } else {
+//         alert("Entrada R$35,00");
+//     } 
+// }
+// else {
+//     alert("Tente Novamente!")
+// }
+
+
+
+
+
+// let semana =  Number(prompt("Digite um numero e eu digo qual é o dia da semana"));
+
+// if(semana === 1){
+//     alert("DOMINGO")
+// }
+// else if(semana == 2){
+//     alert("SEGUNDA-FEIRA");
+// }
+// else if(semana == 3){
+//     alert("TERÇA-FEIRA");
+// }
+// else if(semana == 4){
+//     alert("QUARTA-FEIRA");
+// }
+// else if(semana == 5){
+//     alert("QUINTA-FEIRA");
+// }
+// else if(semana == 6){
+//     alert("SEXTA-FEIRA");
+// }
+// else if(semana == 7){
+//     alert("SABADO");
+// }
+
+// else{
+//     ("Numeoro Invalido: Digite um numero de 1 a 7");
+// }
+
+
+
+
+// let idade = Number(prompt("Digite sua idade"));
+
+// if(idade >= 25 && idade <= 60){
+//     let salario = Number(prompt("Digite o emprestimo desejado"));
+//     if(salario >= 2000){
+//     let emprestimo = Number(prompt("Digite o emprestimo desejado"));
+//     if(emprestimo <=10* salario){
+//         alert("EMPRESTIMO APROVADO")
+//     }else{
+//         alert("EMPRESTIMO RECUSADO, VALOR MUITO ALTO")
+//     }
+
+//     }else{
+//         alert("EMPRESTIMO RECUSADO, PRECISA SER NO MINIMO 2000")
+//     }
+
+// }else{
+//     alert("SEU EMPRESTIMO FOI NEGADO POR CAUSA DA IDADE")
+// }
+
+
+
+
+let numero = Number(prompt("Digite aqui seu numero de celular"));
+
+if(numero[0] == && numero.leght ["9"]){
+    alert("Numero aprovado")
+}
